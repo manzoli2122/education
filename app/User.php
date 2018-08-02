90a52d7498b0704a381a539cb9a463d9b8879067
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-08-02 20:08:18 -0300

    ajustando autenticacao

--- app/User.php
+++ app/User.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable; 
 use InvalidArgumentException;
@@ -10,7 +12,7 @@ use Log;
 
 use Illuminate\Cache\TaggableStore; 
 
-class User extends Authenticatable
+class User extends Authenticatable  implements JWTSubject
 {
 
     use Notifiable;
@@ -64,7 +66,27 @@ class User extends Authenticatable
 
 
 
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    
 
     /**
      * Buscar os usuarios para exibir na datatable
